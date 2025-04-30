14353d38443c2e9b5cced894b8f5f22c541721b3
Purnima Sharma <purnimasharma0924@gmail.com>
2025-04-30 09:27:48 +0545

    commented out supabase integration

--- src/supabase/configSupabase.js
+++ src/supabase/configSupabase.js
@@ -1,9 +1,9 @@
-// src/supabaseClient.js
-import { createClient } from '@supabase/supabase-js';
+// // src/supabaseClient.js
+// import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+// const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+// const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+// const supabase = createClient(supabaseUrl, supabaseKey);
 
-export default supabase;
+// export default supabase;
